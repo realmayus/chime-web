c553800f86e34fe28e8ba179e05f6c666a9407fa
realmayus <realmayus@gmail.com>
2020-06-19 16:08:27 +0200

    Fixed URLs

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import React, {Suspense, useEffect} from "react"
 import {Redirect, Route, Switch} from "react-router-dom"
 import Home from "./pages/Home"
-import {CLIENT_ID, INVITE_URL} from "./constants"
+import {CLIENT_ID, FRONTEND_URL, INVITE_URL} from "./constants"
 import Callback from "./pages/Callback"
 import Dashboard from "./pages/Dashboard"
 import Features from "./pages/Features";
@@ -18,7 +18,7 @@ export default function Main() {
                     <Route exact path="/features" component={Features}/>
                     <Route path="/app" component={Dashboard}/>
                     <Route exact path="/oauth/login" component={() => {
-                        window.location.href = "https://discord.com/api/oauth2/authorize?response_type=token&client_id=" + CLIENT_ID + "&scope=identify&redirect_uri=" + encodeURIComponent("http://" + window.location.hostname + ":" + window.location.port + "/oauth/callback")
+                        window.location.href = "https://discord.com/api/oauth2/authorize?response_type=token&client_id=" + CLIENT_ID + "&scope=identify&redirect_uri=" + encodeURIComponent(FRONTEND_URL + "/oauth/callback")
                         return null
                     }}/>
                     <Route exact path="/oauth/callback" component={Callback}/>
